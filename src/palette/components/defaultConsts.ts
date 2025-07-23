85ee69569349143bdf1346f93ca578608d884f7b
mharkness1 <mharkness.uk@gmail.com>
2025-07-23 07:05:02 +0100

    B: palette constants defined

---
+++ src/palette/components/defaultConsts.ts
@@ -0,0 +1,5 @@
+export const defaultGrayTolerance: number = 10;
+export const defaultBlackTolerance: number = 10;
+export const defaultWhiteTolerance: number = 10;
+export const defaultStepSize: number = 30;
+export const defaultSpectrumSize: number = 6;
